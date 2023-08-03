6238ba2a55180c57734ca225dea3e2ade5d352f8
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-08-03 16:54:08 +0100

    fix: add scope

--- main.tf
+++ main.tf
@@ -153,6 +153,7 @@ resource "azurerm_role_definition" "this" {
 
 resource "azurerm_role_assignment" "this" {
   for_each             = var.role_assignments
+  scope                = azurerm_management_group.this.id
   principal_id         = each.value.principal_id
   role_definition_id   = each.value.role_definition_id != "" ? each.value.role_definition_id : null
   role_definition_name = each.value.role_definition_name != "" ? each.value.role_definition_name : null
