0575e39e755fee9853a461ee2504325392f67bf8
Jared Holgate <jaredholgate@microsoft.com>
2023-12-11 20:43:36 +0000

    Add version and add prefix names

--- examples/alzreference/main.tf
+++ examples/alzreference/main.tf
@@ -6,6 +6,7 @@ resource "random_pet" "this" {
 module "naming" {
   source = "Azure/naming/azurerm"
   suffix = [random_pet.this.id]
+  prefix = ["avm-ptn-alz"]
 }
 
 module "alz_management_resources" {
