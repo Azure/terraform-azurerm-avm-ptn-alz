29f0e274450cbc591cfdfac64c5f1389494589bc
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-10-15 15:26:44 +0100

    fix: alz provider 0.15.2

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     alz = {
       source  = "azure/alz"
-      version = ">= 0.15.1, < 1.0.0"
+      version = ">= 0.15.2, < 1.0.0"
     }
     azapi = {
       source  = "azure/azapi"
