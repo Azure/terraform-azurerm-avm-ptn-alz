1914bfa27ad7574ead4fb4bfa13f51bff7e1c1e2
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-10-31 13:57:52 +0000

    chore: lint

--- examples/policy-assignment-modification-with-custom-lib/terraform.tf
+++ examples/policy-assignment-modification-with-custom-lib/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = "~> 1.8"
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~> 2.0"
+    alz = {
+      source  = "azure/alz"
+      version = "~> 0.16"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
