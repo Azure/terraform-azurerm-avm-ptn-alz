182c20a0abd586d5deac91f67e8111fff04f4b38
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-20 10:54:26 +0000

    feat: assign permissions override (#168)
    * feat: add definition assign permissions override
    
    * feat: add assign permissions default
    
    * feat: alz provider 0.17
    
    * docs: make docs
    
    * feat: add authorization failed retry
    
    * chore: simplify provider constraint
    
    * fix: review comments

--- examples/default/terraform.tf
+++ examples/default/terraform.tf
@@ -1,9 +1,20 @@
 terraform {
   required_version = "~> 1.8"
   required_providers {
+    alz = {
+      source  = "azure/alz"
+      version = "~> 0.17"
+    }
     azapi = {
       source  = "azure/azapi"
       version = "~> 2.0, >= 2.0.1"
     }
   }
+}
+
+provider "alz" {
+  library_references = [{
+    path = "platform/alz"
+    ref  = "2025.01.0"
+  }]
 }
