6976fa13be647fa6cd5372b7f018d74c36b315c1
Arjen Huitema <arjenhuitema@microsoft.com>
2025-06-05 15:07:43 +0200

    feat: add ALZ + AMBA example (#217)
    * feat: add ALZ + AMBA example with necessary configurations and documentation
    
    * feat: update ALZ + AMBA example with enhanced configurations and documentation
    
    * fix: update alz provider version constraint for compatibility
    
    * pre-commit
    
    * feat: add amba architecture definition JSON file

---
+++ examples/amba/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    alz = {
+      source  = "Azure/alz"
+      version = "~> 0.17, >= 0.17.4"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 2.2"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
