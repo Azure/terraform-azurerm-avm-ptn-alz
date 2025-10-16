28216084b54090fef0b2c6459e5e3f3b07ab8e60
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-10-16 11:09:01 +0100

    chore: bump alz provider to 0.20

--- terraform.tf
+++ terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     alz = {
       source  = "azure/alz"
-      version = "~> 0.19"
+      version = "~> 0.20"
     }
     azapi = {
       source  = "Azure/azapi"
