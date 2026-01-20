99b2c50a5b04cd82243f58f60e7038cc5ed9159b
Jared Holgate <jaredholgate@microsoft.com>
2026-01-20 11:17:45 +0000

    feat: improve subscription placement validation at plan time

--- variables.tf
+++ variables.tf
@@ -623,6 +623,11 @@ DESCRIPTION
     error_message = "All subscription ids must be valid UUIDs."
     condition     = alltrue([for v in var.subscription_placement : can(regex("^[a-f\\d]{4}(?:[a-f\\d]{4}-){4}[a-f\\d]{12}$", v.subscription_id))])
   }
+
+  validation {
+    error_message = "Each subscription ID can only be specified once in the subscription_placement variable."
+    condition     = length(var.subscription_placement) == length(distinct([for v in var.subscription_placement : v.subscription_id]))
+  }
 }
 
 variable "subscription_placement_destroy_behavior" {
