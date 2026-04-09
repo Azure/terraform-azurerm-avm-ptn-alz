8ec3f9606ac1915d8bdaf0d7b8a57c52c8490ce4
Jared Holgate <jaredholgate@microsoft.com>
2026-04-09 21:50:23 +0100

    refactor: rename enabled to assignment_creation_enabled for policy assignments

--- variables.tf
+++ variables.tf
@@ -392,7 +392,6 @@ DESCRIPTION
 variable "policy_assignments_to_modify" {
   type = map(object({
     policy_assignments = map(object({
-      enabled          = optional(bool, true)
       enforcement_mode = optional(string, null)
       identity         = optional(string, null)
       identity_ids     = optional(list(string), null)
@@ -418,6 +417,7 @@ variable "policy_assignments_to_modify" {
           not_in = optional(set(string), null)
         })), [])
       })))
+      assignment_creation_enabled = optional(bool, true)
     }))
   }))
   default     = {}
@@ -429,7 +429,6 @@ The key is the id of the management group. The value is an object with a single 
 The `policy_assignments` value is a map of policy assignments to modify.
 The key of this map is the assignment name, and the value is an object with optional attributes for modifying the policy assignments.
 
-- `enabled` - (Optional) Whether the policy assignment is created or not. Defaults to `true`. This is a convenience property for small scale deployments, the recommended approach is to update your custom library to exclude the policy assignment.
 - `enforcement_mode` - (Optional) The enforcement mode of the policy assignment. Possible values are `Default` and `DoNotEnforce`.
 - `identity` - (Optional) The identity of the policy assignment. Possible values are `SystemAssigned` and `UserAssigned`.
 - `identity_ids` - (Optional) A set of ids of the user assigned identities to assign to the policy assignment.
@@ -450,6 +449,8 @@ The key of this map is the assignment name, and the value is an object with opti
     - `kind` - (Required) The kind of the selector.
     - `in` - (Optional) A set of strings to include in the selector.
     - `not_in` - (Optional) A set of strings to exclude from the selector.
+- `assignment_creation_enabled` - (Optional) Whether the policy assignment is created or not. Defaults to `true`. This is a convenience property for small scale deployments, the recommended approach is to update your custom library to exclude the policy assignment.
+
 DESCRIPTION
 }
 
