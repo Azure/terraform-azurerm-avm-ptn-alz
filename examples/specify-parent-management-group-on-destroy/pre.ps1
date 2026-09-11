1654a6e1101361d30662f9bb0557d70c142eec7e
Qi Zhang <v-zqi@microsoft.com>
2026-09-11 17:24:50 +0800

    Reapply "fix: convert lifecycle shell hooks to PowerShell (#322)"
    This reverts commit f459f8f78857c17490d16bf2bdec2610b0b5c0a6.

---
+++ examples/specify-parent-management-group-on-destroy/pre.ps1
@@ -0,0 +1,25 @@
+#!/usr/bin/env pwsh
+# Sets up the environment for the specify-parent-management-group-on-destroy example.
+
+Set-StrictMode -Version 3.0
+$ErrorActionPreference = 'Stop'
+
+$randomPrefix = Get-Random -Minimum 0 -Maximum 32768
+$libDir = Join-Path $PSScriptRoot 'lib'
+
+if (Test-Path -LiteralPath $libDir -PathType Container) {
+    Push-Location -LiteralPath $libDir
+    try {
+        terraform init
+        if ($LASTEXITCODE -ne 0) { throw "terraform init failed with exit code $LASTEXITCODE." }
+
+        terraform apply -auto-approve "-var=prefix=$randomPrefix"
+        if ($LASTEXITCODE -ne 0) { throw "terraform apply failed with exit code $LASTEXITCODE." }
+    }
+    finally {
+        Pop-Location
+    }
+}
+
+$tfvarsPath = Join-Path $PSScriptRoot 'terraform.tfvars'
+[System.IO.File]::WriteAllText($tfvarsPath, ('prefix = "{0}"' -f $randomPrefix) + "`n")
